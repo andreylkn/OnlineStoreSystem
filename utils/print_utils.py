eafd89d4ed01de378c2e2297f55e4662e032d6a9
Yukimee <leeling9696@gmail.com>
2025-02-20 18:50:31 +1300

    update main file
    added customer features selection prompt

--- utils/print_utils.py
+++ utils/print_utils.py
@@ -11,6 +11,19 @@ def print_admin_menu():
 def print_customer_menu():
     print("------------------- Customer Menu ----------------------")
     print("1. Logout")
+    print("1. View Categories")
+    print("2. View Shopping Cart")
+    print("3. Logout")
+
+def print_categories_menu():
+    print("------------------- Categories Menu ----------------------")
+
+def print_products_menu():
+    print("------------------- Products Menu ----------------------")
+
+def print_cart_menu():
+    print("------------------- Shopping Cart ----------------------")
 
 def print_invalid_choice():
     print("Invalid choice. Please try again.")
+
